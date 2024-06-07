544cfdc1e501173ab3ba1b7df9f454cd12ece905
ann573 <165071631+ann573@users.noreply.github.com>
2024-06-07 21:38:48 +0700

    Update script.js

--- script.js
+++ script.js
@@ -34,6 +34,13 @@ const app ={
 
         },
         {
+          name: 'You don\'t know my name',
+          image: './image/houselak.jpg',
+          path: './music/you_do_know_me.mp3',
+          singer: 'Binz.'
+
+      },
+        {
             name: 'Haru Haru',
             image: './image/haru.jpg',
             path: './music/haru.mp3',
@@ -101,7 +108,6 @@ const app ={
         const cdWidth = cd.offsetWidth;
     
         // Xử lý CD quay / dừng
-        // Handle CD spins / stops
         const cdThumbAnimate = cdThumb.animate([{ transform: "rotate(360deg)" }], {
           duration: 10000, // 10 seconds
           iterations: Infinity
@@ -109,7 +115,6 @@ const app ={
         cdThumbAnimate.pause();
     
         // Xử lý phóng to / thu nhỏ CD
-        // Handles CD enlargement / reduction
         document.onscroll = function () {
           const scrollTop = window.scrollY || document.documentElement.scrollTop;
           const newCdWidth = cdWidth - scrollTop;
@@ -119,7 +124,6 @@ const app ={
         };
     
         // Xử lý khi click play
-        // Handle when click play
         playBtn.onclick = function () {
           if (_this.isPlaying) {
             audio.pause();
@@ -129,7 +133,6 @@ const app ={
         };
     
         // Khi song được play
-        // When the song is played
         audio.onplay = function () {
           _this.isPlaying = true;
           player.classList.add("playing");
@@ -137,7 +140,6 @@ const app ={
         };
     
         // Khi song bị pause
-        // When the song is pause
         audio.onpause = function () {
           _this.isPlaying = false;
           player.classList.remove("playing");
@@ -145,7 +147,6 @@ const app ={
         };
     
         // Khi tiến độ bài hát thay đổi
-        // When the song progress changes
         audio.ontimeupdate = function () {
           if (audio.duration) {
             const progressPercent = Math.floor(
@@ -156,14 +157,12 @@ const app ={
         };
     
         // Xử lý khi tua song
-        // Handling when seek
         progress.onchange = function (e) {
           const seekTime = (audio.duration / 100) * e.target.value;
           audio.currentTime = seekTime;
         };
     
         // Khi next song
-        // When next song
         nextBtn.onclick = function () {
           if (_this.isRandom) {
             _this.playRandomSong();
@@ -176,7 +175,6 @@ const app ={
         };
     
         // Khi prev song
-        // When prev song
         prevBtn.onclick = function () {
           if (_this.isRandom) {
             _this.playRandomSong();
@@ -189,7 +187,6 @@ const app ={
         };
     
         // Xử lý bật / tắt random song
-        // Handling on / off random song
         randomBtn.onclick = function (e) {
           _this.isRandom = !_this.isRandom;
           _this.setConfig("isRandom", _this.isRandom);
@@ -197,7 +194,6 @@ const app ={
         };
     
         // Xử lý lặp lại một song
-        // Single-parallel repeat processing
         repeatBtn.onclick = function (e) {
           _this.isRepeat = !_this.isRepeat;
           _this.setConfig("isRepeat", _this.isRepeat);
@@ -205,7 +201,6 @@ const app ={
         };
     
         // Xử lý next song khi audio ended
-        // Handle next song when audio ended
         audio.onended = function () {
           if (_this.isRepeat) {
             audio.play();
@@ -215,13 +210,11 @@ const app ={
         };
     
         // Lắng nghe hành vi click vào playlist
-        // Listen to playlist clicks
         playlist.onclick = function (e) {
           const songNode = e.target.closest(".song:not(.active)");
     
           if (songNode || e.target.closest(".option")) {
             // Xử lý khi click vào song
-            // Handle when clicking on the song
             if (songNode) {
               _this.currentIndex = Number(songNode.dataset.index);
               _this.loadCurrentSong();
@@ -230,7 +223,6 @@ const app ={
             }
     
             // Xử lý khi click vào song option
-            // Handle when clicking on the song option
             if (e.target.closest(".option")) {
             }
           }
@@ -278,26 +270,21 @@ const app ={
       },
       start: function () {
         // Gán cấu hình từ config vào ứng dụng
-        // Assign configuration from config to application
         this.loadConfig();
     
         // Định nghĩa các thuộc tính cho object
-        // Defines properties for the object
         this.defineProperties();
     
         // Lắng nghe / xử lý các sự kiện (DOM events)
-        // Listening / handling events (DOM events)
         this.handleEvents();
     
         // Tải thông tin bài hát đầu tiên vào UI khi chạy ứng dụng
-        // Load the first song information into the UI when running the app
         this.loadCurrentSong();
     
         // Render playlist
         this.render();
     
         // Hiển thị trạng thái ban đầu của button repeat & random
-        // Display the initial state of the repeat & random button
         randomBtn.classList.toggle("active", this.isRandom);
         repeatBtn.classList.toggle("active", this.isRepeat);
       }
